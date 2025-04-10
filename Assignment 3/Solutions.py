650cce995478d9ec6481bb431f8255786217d5c2
Asiakiba <asiakibasophia@gmail.com>
2025-04-10 05:36:53 -0800

    Solutions_5240100733_2025.py
    solution to assiagnment

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,31 +6,64 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-
+text="programming"
+reversed_text=text[::-1]
+print(Reversed string(slicing):'"reversed_text)
+Reply to the email directly,view it on GitHub,or unsubscribe,
+you are receiving this because you commented
 
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+Full_name=input("Enter your full name:")
+name_parts=full_name,split()
+initials=[name[0],upper()+";for name in name_part]
+          print("",join(initial))
 
+        _
+          Reply to this email directly,View it on 
+          GitHub,or unsubscribe.
+You are receiving this because you commented
 
 
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
-
-
+text=input("Enter a string:")
+text=text.lower()
+if text==text[::-1]:
+  print(f"{text}'is not a palindrome.")
+  else
+  print(f"'{text}'is not a palindrome.")
+  _
+  Reply to this email directly,view itt on
+  GitHub,or unsubscrib.
+  you are receiving this because you commented.
 
 """
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
-
+Sentence=input("Enter a sentence:")
+words=sentence.split()
+World_count=len(word)
+print("number of words in the sentence:"x,word_count)
 
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+text="this is a string and it is an example."
+modified_text=text.replace("is","was",)
+print ("modified string :",modified_text)
+_
+Reply to this email directly,View it on
+GitHub,or unsubscribe.
+you are receiving this because you commented
+
+
+
