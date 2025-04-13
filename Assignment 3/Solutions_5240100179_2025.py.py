bda38242d084d61b47d3713df638eb56ed83dd3b
awatey <joshuaawatey971@gmail.com>
2025-04-12 18:04:37 -0700

    solutions-to-the-Assignment-3

---
+++ Assignment 3/Solutions_5240100179_2025.py.py
@@ -0,0 +1,75 @@
+"""
+Solutions to assignment 3
+"""
+
+"""
+1.Write a Python program to reverse the string "Programming". Print the reversed string.
+Hint: Use string slicing or a loop.
+"""
+
+original_string = "Programming"
+
+
+reversed_string = original_string[::-1]
+
+
+print("Reversed string:", reversed_string)
+
+
+
+"""
+2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
+Example: Input: "john doe", Output: "J.D."
+"""
+full_name = input("Enter your full name: ")
+
+name_parts = full_name.strip().split()
+
+initials = '.'.join([part[0].upper() for part in name_parts]) + '.'
+
+print("Initials:", initials)
+
+
+
+"""
+3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
+and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
+"""
+text = input("Enter a string: ")
+
+text = text.lower()
+
+if text == text[::-1]:
+    print("Yes, it's a palindrome!")
+else:
+    print("No, it's not a palindrome.")
+
+
+
+"""
+4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
+Hint: Use the split() method to break the string into words.
+"""
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+word_count = len(words)
+
+print("Number of words in the sentence:", word_count)
+
+
+
+"""
+5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
+is an example." Print the modified string.
+"""
+text = "This is a string and it is an example."
+
+words = text.split()
+
+new_words = ['was' if word == 'is' else word for word in words]
+
+modified_text = ' '.join(new_words)
+
+print("Modified string:", modified_text)
