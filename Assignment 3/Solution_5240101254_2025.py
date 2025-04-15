e2130969081dfe4bccbeed3403ed87be7d7661dd
Amoadu Emmanuel <emmanuelamoadu71@gmail.com>
2025-04-15 11:40:32 +0000

    Update and rename Solutions.py to Solution_5240101254_2025.py

--- Assignment 3/Solution_5240101254_2025.py
+++ Assignment 3/Solution_5240101254_2025.py
@@ -6,14 +6,17 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-
-
+main = "programming"
+reversed_main = main[::-1]
+print(reversed_main)
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
-
+full_name = input("Enter your full name: ")
+initials = ".".join(name[0].upper() for name in full_name.split())
+print(f"{initials}.")
 
 
 """
@@ -28,9 +31,18 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 Hint: Use the split() method to break the string into words.
 """
 
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+word_count = len(words)
+print(f"The sentence contains {word_count} words.")
+
 
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+
+phrase = "This is a string and it is an example."
+phrase = phrase.replace("is", "was")
+print(phrase)
