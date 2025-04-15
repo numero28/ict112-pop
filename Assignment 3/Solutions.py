6a9f1664c8570cb372790a730caefb1c4dcbed86
Jephthah-543 <jephthah940@gmail.com>
2025-04-15 22:51:45 +0000

    Solutions_5240100176_2025.py
    Solutions to assignment questions

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -7,12 +7,19 @@ Solutions to assignment 3
 Hint: Use string slicing or a loop.
 """
 
+string_to_reverse = "Programming"
+reversed_string = string_to_reverse[::-1]
+print(reversed_string)
 
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+
+full_name = input("Enter your full name: ")
+initials = '.'.join(part[0].upper() for part in full_name.split())
+print(f"{initials}.")
 
 
 
@@ -21,6 +28,15 @@ Example: Input: "john doe", Output: "J.D."
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
 
+def is_palindrome(s):
+    return s == s[::-1]
+
+input_string = "radar"
+if is_palindrome(input_string):
+    print(f'"{input_string}" is a palindrome.')
+else:
+    print(f'"{input_string}" is not a palindrome.')
+
 
 
 """
@@ -28,9 +44,15 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 Hint: Use the split() method to break the string into words.
 """
 
+sentence = input("Enter a sentence: ")
+word_count = len(sentence.split())
+print(f"Number of words: {word_count}")
 
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+original_string = "This is a string and it is an example."
+modified_string = original_string.replace("is", "was")
+print(modified_string
