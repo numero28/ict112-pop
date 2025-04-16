6f3fb8cf37952e7f511185dcdde2e3d094d0703d
francis272676 <boatengfrancis272002apl@gmail.com>
2025-04-16 17:01:05 +0100

    Solutions_5240100739_2025H.py
    SOLUTIONS

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,7 +6,7 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-reverse_string "Programming"
+reverse_string= "Programming"
 original = "Programming"
 reversed_string = original[::-1]
 print("1. Reversed string:", reversed_string)
@@ -14,7 +14,6 @@ print("1. Reversed string:", reversed_string)
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
-print initials in uppercase
 full_name = input("2. Enter your full name: ").strip()
 name_parts = full_name.split()
 initials = ".".join([name[0].upper() for name in name_parts]) + "."
@@ -33,7 +32,7 @@ else:
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
- Count number of words in a sentence
+
 sentence = input("4. Enter a sentence: ")
 words = sentence.split()
 print("Number of words:", len(words))
@@ -41,7 +40,6 @@ print("Number of words:", len(words))
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
-replace all occurrences of "is" with "was"
 original_text = "This is a string and it is an example."
 modified_text = original_text.replace("is", "was")
 print("5. Modified string:", modified_text)
