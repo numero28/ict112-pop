9ecd6e528953e242164bcfdddf97d6990ad9f7de
tasha-luv88 <natashalove307@gmail.com>
2025-04-17 13:33:25 +0000

    Solutions_5240101203_2025.py

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,6 +6,12 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
+s = "programming"
+reversed_s = ""
+for char in s:
+    reversed_s = char + reversed_s
+print(reversed_s)
+
 
 
 
@@ -13,6 +19,9 @@ Hint: Use string slicing or a loop.
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+full_name = input("Enter your full name: ")
+initials = ".".join(name[0].upper() for name in full_name.split())
+print(f"{initials}.")
 
 
 
@@ -20,6 +29,15 @@ Example: Input: "john doe", Output: "J.D."
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
+def is_palindrome(s):
+    s = s.replace(" ", "").lower()
+    return s == ""
+s = input("Enter a string: ")
+if is_palindrome(s):
+    print(f"'{s}' is a palindrome.")
+else:
+    print(f"'{s}' is not a palindrome.")
+
 
 
 
@@ -27,6 +45,12 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+word_count = len(words)
+print(f"The sentence contains {word_count} words.")
+
+
 
 
 
@@ -34,3 +58,6 @@ Hint: Use the split() method to break the string into words.
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+s = "This is a string and it is an example."
+s = s.replace("is", "was")
+print(s)
