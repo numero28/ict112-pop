88ec15572c64a664bb21b38a942400036b801bb0
Purkabong-2005 <purkabonganita@gmail.com>
2025-04-18 17:24:54 +0000

    Solutions_5240100966_2025.py
    assignment three solutions by Purkabong Anita

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,13 +6,21 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-
+# Question 1: Reverse the string "Programming"
+text = "Programming"
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
 
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+# Question 2: Get user’s initials in uppercase from their full name
+full_name = input("Enter your full name: ")
+names = full_name.strip().split()  # splits the full name into separate words
+initials = ".".join([name[0].upper() for name in names]) + "."
+print("Initials:", initials)
 
 
 
@@ -20,6 +28,12 @@ Example: Input: "john doe", Output: "J.D."
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
+# Question 3: Check if the given word is a palindrome
+word = input("Enter a word: ")
+if word.lower() == word[::-1].lower():
+    print("It's a palindrome.")
+else:
+    print("Not a palindrome.")
 
 
 
@@ -27,6 +41,11 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
+# Question 4: Count the number of words in a sentence
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+print("Word count:", len(words))
+
 
 
 
@@ -34,3 +53,7 @@ Hint: Use the split() method to break the string into words.
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+# Question 5: Replace "is" with "was" in the given text
+text = "This is a string and it is an example."
+modified_text = text.replace("is", "was")
+print("Modified text:", modified_text)
