4340eb8d28bbf9dd4cb79c4fd3826814fd438796
Tijani1868 <tijanisaviour8@gmail.com>
2025-04-19 20:36:49 +0000

    Solutions_5240101237_2025.py

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,6 +6,12 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
+t = "programming"
+reversed_s = ""
+for char in s:
+    reversed_s = char + reversed_s
+print(reversed_s)
+
 
 
 
@@ -14,12 +20,26 @@ Hint: Use string slicing or a loop.
 Example: Input: "john doe", Output: "J.D."
 """
 
+full_name = input("Enter your full name: ")
+initials = ".".join(name[0].upper() for name in full_name.split())
+print(f"{initials}.")
+
 
 
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
+
+def is_palindrome(t):
+    t = t.replace(" ", "").lower()
+    return t == ""
+ t = input("Enter a string: ")
+if is_palindrome(t):
+    print(f"'{t}' is a palindrome.")
+else:
+    print(f"'{t}' is not a palindrome.")
+
 
 
 
@@ -28,9 +48,18 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 Hint: Use the split() method to break the string into words.
 """
 
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+word_count = len(words)
+print(f"The sentence contains {word_count} words.")
+
 
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+
+t = "This is a string and it is an example."
+t = t.replace("is", "was")
+print(t)
