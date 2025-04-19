c78aa8f4aa98849c330360061a551933b4b11f3a
Abambilla <patrickalaribaabambilla@gmail.com>
2025-04-19 08:54:19 -0700

    Solution_5240100128_2025.py
    I have added solution to assignment3

---
+++ Solutions_5240100128_2025.py.py
@@ -0,0 +1,17 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\USER\ict112-pop\Solutions_5240100128_2025.py.py
+gnimmargorP
+Enter your full name: richard ansah
+R.A.
+Enter a string: ama
+It is a palindrome.
+Enter a sentence: asj
+Number of words: 1
+Thwas was a string and it was an example.
+>>> dsd
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    dsd
+NameError: name 'dsd' is not defined
