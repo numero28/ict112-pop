da00924c07bb11b1283a9e4e47c99893bc43b50b
Histins11 <hastingsasare370@gmail.com>
2025-04-20 06:00:10 +0000

    Sir this is my solutions

--- Assignment 3/Solutions_5240100335_2025.py
+++ Assignment 3/Solutions_5240100335_2025.py
@@ -1,11 +1,16 @@
 """
+Solutions to assignment 3
+"""
+
+"""
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-
 string = "Programming"
 reversed_string = string[::-1]
 print(reversed_string) 
+
+
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
@@ -19,6 +24,7 @@ def print_initials(full_name):
 full_name = input("Enter your full name: ")
 print_initials(full_name)
 
+
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
@@ -30,7 +36,9 @@ if word == reverseword:
 else:
     print("it's not a palindrome")
 
-    """
+
+
+"""
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
@@ -38,6 +46,8 @@ sentence = input("Enter a sentence: ")
 sentenceSplit = sentence.split()
 wordSplit =len(sentenceSplit)
 print("The number of words in the sentence is ", wordSplit)
+
+
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
