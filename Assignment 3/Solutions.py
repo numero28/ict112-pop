517221085679c76429bdb392c1d49d52780d3c73
FrimpongKyeiJoseph3734 <quaminana343@gmail.com>
2025-04-20 03:29:25 -0700

    Solutions_5240100337_2025.py
    I like programming

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -6,27 +6,46 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
-
+ Define the string
+original_string = "Programming"
+ Reverse the string using slicing
+reversed_string = original_string[::-1]
+ Print the reversed string
+print(reversed_string)
 
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
-
+def print_initial(full_name):
+  names = full_name.split()
+  initials = ".".join(name[0].upper() for name in names)
+  print(f"{initials}.")
+  full_name = input("Enter your full name: ")
+  print_initials(full_name)
 
 
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
-
+word = input("Enter a word: ")
+reverseword = word[::-1]
+if word == reverseword:
+  print("it's a palindrome")
+else:
+  print("it's not a palindrome"
 
 
 """
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
+sentence = input("Enter a sentence: ")
+sentenceSplit = sentence.split()
+wordSplit = len(sentenceSplit)
+print("The number of words in the sentence is ", wordSplit)
 
 
 
@@ -34,3 +53,6 @@ Hint: Use the split() method to break the string into words.
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+sentence = "This is a string and it is an example."
+replaceWordv = sentence.replace("is","was")
+print("Modified string: ", replaceWord)
