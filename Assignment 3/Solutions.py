66e5e0f53ea03e5504511d8cc2722652c0a83474
Kojo-12345 <ek5603065@gmail.com>
2025-04-24 19:54:44 +0100

    Solutions_<5240101357>_2025.py
    hello word

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -1,36 +1,69 @@
 """
-Solutions to assignment 3
-"""
-
-"""
-1.Write a Python program to reverse the string "Programming". Print the reversed string.
-Hint: Use string slicing or a loop.
-"""
+Solutions to assignment
 
 
 
-"""
-2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
-Example: Input: "john doe", Output: "J.D."
-"""
+
 
 
 
 """
-3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
-and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
+original = "Programming"def get_initials(full_name):
+  
+    name_parts = full_name.strip().split()
+  
+    initials = [name[0].upper() for name in name_parts if name]
+        return '.'.join(initials) + '.'
+
+
+
+
+
+
+
+
+def get_initials(full_name):
+    name_parts = full_name.strip().split()
+    initials = [name[0].upper() for name in name_parts if name
+    return '.'.join(initials) + '.'
+user_name = input("Enter your full name: ")
+print("Initials:", get_initials(user_name))
+
+
+def is_palindrome(s):
+
+
+
+
+
+  
+  
+    s = s.replace(" ", "").lower()
+    return s == s[::-1]
+user_input = input("Enter a string: ")
+if is_palindrome(user_input):
+    print(f"'{user_input}' is a palindrome.")
+else:
+    print(f"'{user_input}' is not a palindrome.")
+
+
+
+
+
+
+sentence = input("Enter a sentence:
+words = sentence.split()
+
+
+
+
+
+
+
+""
+text = "This is a string and it is an example."
+modified_text = text.replace("is", "was")
+print(modified_text)
+.
 """
 
-
-
-"""
-4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
-Hint: Use the split() method to break the string into words.
-"""
-
-
-
-"""
-5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
-is an example." Print the modified string.
-"""
